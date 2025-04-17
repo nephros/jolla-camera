759ab551717dd0cf01f8946fd98012a397b3ccff
Pekka Vuorela <pekka.vuorela@jolla.com>
2025-04-17 15:18:51 +0300

    [camera] Move quickaction translations here. JB#63000
    Was using previous lipstick-jolla to ease working with earlier
    release but we can now move it here.

---
+++ quickactions/translations.js
@@ -0,0 +1,8 @@
+// add translations for quick actions
+function qsTrIdString() {
+    //% "Take a selfie"
+    QT_TRID_NOOP("settings_quickaction-la-take_a_selfie")
+    //: Abbreviated form of settings_quickaction-la-take_a_selfie
+    //% "Selfie"
+    QT_TRID_NOOP("settings_quickaction-la-take_a_selfie_short")
+}
